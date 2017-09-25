14a3287efef7a2ec381a650168a92ba88a0fdfb3
Álvaro Peralta <alvaroperalta@MacBook-Air-de-Alvaro.local>
2017-09-25 20:32:31 +0200

    Sumatorio

--- src/main/scala/Clase1B.scala
+++ src/main/scala/Clase1B.scala
@@ -1,7 +1,15 @@
-  object Clase1B{
-    def main(args: Array[String]): Unit = {
-      Clase1.main(null)
+import com.sun.tools.javac.code.Attribute.Array
 
-    }
+object Clase1B{
+  def main(args: Array[String]): Unit = {
+    Sumar(2,2)
   }
+
+  def Sumar(var1:Integer, var2:Integer): Integer = {
+
+    val result = var1 + var2
+    println(result)
+    result
+  }
+}
 
