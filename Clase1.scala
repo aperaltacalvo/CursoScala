433e1a4e3ba7a1a635217638d59fa70f95b0ae32
aperaltacalvo <alvaro.peralta.calvo@gmail.com>
2017-09-25 20:19:43 +0200

    first commit
    first commit of course

---
+++ Clase1.scala
@@ -0,0 +1,12 @@
+object Clase1{
+    def main(args: Array[String]): Unit = {
+      holaMudo
+    }
+
+    def holaMudo: Unit = {
+      val saludo: String = "Hola"
+
+      println(saludo)
+    }
+
+  }
