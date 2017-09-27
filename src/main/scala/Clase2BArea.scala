f2a9ef0fc146d0054b4550be3006a3451964b3d7
Álvaro Peralta <alvaroperalta@MacBook-Air-de-Alvaro.local>
2017-09-27 20:07:15 +0200

    Area exercise

---
+++ src/main/scala/Clase2BArea.scala
@@ -0,0 +1,18 @@
+object Clase2BArea {
+
+  def main(args: Array[String]): Unit = {
+    Area(10)
+  }
+
+  def Area(var1:Double): Double = {
+
+    val result = 3.14159 * Square(var1)
+    println(result)
+    result
+  }
+
+  def Square(var1:Double): Double ={
+    BigDecimal(var1).pow(2).doubleValue()
+
+  }
+}
