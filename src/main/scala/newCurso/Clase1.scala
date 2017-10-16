4eb6cd793a80a78062f5c0c5cf27f5ebde1ba635
Álvaro Peralta <alvaroperalta@MacBook-Air-de-Alvaro.local>
2017-10-16 20:38:07 +0200

    Examples with maps and lists

--- src/main/scala/newCurso/Clase1.scala
+++ src/main/scala/newCurso/Clase1.scala
@@ -1,5 +1,7 @@
 package newCurso
 
+import scala.annotation.tailrec
+
 object Clase1 {
 
   def sum(x:Int , y : Int):Int = {
@@ -19,11 +21,58 @@ object Clase1 {
     x/y
   }
 
+  def operar (f:(Int,Int)=>Int, x:Int, y:Int):Int = {
+    f(x,y)
+  }
+
+  val l  = List(1,2,3)
+  val l1 = List(1,2,3)
+
+  val l2 = List(l,l1)
+
+  def factB( value : Int ):Int = {
+    @tailrec
+    def factB_(cont: Int, value:Int): Int = {
+
+      if (cont == 0) {
+        value
+
+      } else {
+        factB_(cont - 1, (value * (cont)))
+
+      }
+
+    }
+
+    factB_( value-1, value)
+
+  }
+
+
+
+
   def main(args: Array[String]): Unit = {
     println(sum(2,2))
     println(rest(2,2))
     println(div(2,2))
     println(mult(2,2))
+    println(operar(sum,1,2))
+
+    println(l.map(x => x+1))
+    println(l.foreach(x => x+1))
+    println(l2.flatMap(x => x))
+
+    println(l:::l1)
+    println(1::l1)
+    //Pattern Matching
+
+   println( l1 match{
+      case x :: i :: j :: z => x+i+j
+      case _ => 5
+
+    })
   }
+
+
 
 }
